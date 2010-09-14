8b2e8bdd09fe44b7c702ec72af3cc674f8dcb436
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-09-14 14:26:38 +0200

    extract host:port from logcat and use adb forward
    This modification allows you to start this shell script with:
    
    $ . sl4a-start.sh
    
    and you will get AP_HOST and AP_PORT in enviroment, so you can
    run examples using locally installed perl

--- sl4a-start.sh
+++ sl4a-start.sh
@@ -1,7 +1,31 @@
-#!/bin/sh -x
+#!/bin/sh
 
 # see http://code.google.com/p/android-scripting/wiki/RemoteControl
 
+# start with:
+#
+# $ . sl4a-start.sh
+#
+# and you will get AP_HOST and AP_PORT in enviroment
+
 adb shell \
 am start -a com.googlecode.android_scripting.action.LAUNCH_SERVER -n com.googlecode.android_scripting/.activity.ScriptingLayerServiceLauncher --ez com.googlecode.android_scripting.extra.USE_PUBLIC_IP true
-adb logcat | grep sl4a.JsonRpcServer
+
+sleep 1
+
+server=`adb logcat -d | grep sl4a.JsonRpcServer | grep "Bound to" | sed 's/^.*Bound to //' | tr -d '\r' | tail -1`
+
+echo "SL4A bound to $server"
+
+host=`echo $server | cut -d: -f1`
+port=`echo $server | cut -d: -f2`
+
+if [ "$host" == "127.0.0.1" ] ; then
+	echo "not public IP, forwarding port $port"
+	adb forward tcp:$port tcp:$port
+fi
+
+export AP_HOST=$host
+export AP_PORT=$port
+
+echo "AP_HOST=$AP_HOST AP_PORT=$AP_PORT"
